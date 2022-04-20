31658a32c9b146af8992df0278b093074c294ece
jfdsouza988 <juliosouza.rs@gmail.com>
2022-04-20 10:39:30 -0300

    adicionado saga middleware para checagem de estoque

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,15 +1,16 @@
 import { Reducer } from "redux";
 import produce from 'immer';
-import { ICartState } from "./types";
+import { ActionsTypes, ICartState } from "./types";
 
 const INITIAL_STATE: ICartState = {
-  items: []
+  items: [],
+  failedStockCheck: [],
 };
 
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
   return produce(state, draft => {
     switch (action.type) {
-      case 'ADD_PRODUCT_TO_CART': {
+      case ActionsTypes.addProductToCartSuccess: {
         const { product } = action.payload;
 
         const productInCartIndex = draft.items.findIndex(item =>
@@ -25,6 +26,10 @@ const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
           });
         }
 
+        break;
+      }
+      case ActionsTypes.addProductToCartFailure: {
+        draft.failedStockCheck.push(action.payload.productId);
         break;
       }
       default: {
