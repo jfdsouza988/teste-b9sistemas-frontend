50c22b428c8a315d280e8bec0d9467797a801598
jfdsouza988 <juliosouza.rs@gmail.com>
2022-04-20 00:37:57 -0300

    iniciando com redux e redux saga

---
+++ src/components/Catalog.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+
+import api from '../services/api';
+import { IProduct } from '../store/modules/cart/types';
+import CatalogItem from './CatalogItem';
+
+export default function Catalog() {
+  const [catalog, setCatalog] = useState<IProduct[]>([]);  
+
+  useEffect(() => {
+    api.get('product').then(response => {
+      setCatalog(response.data);
+    })
+  }, []);  
+
+  return (
+    <main>
+      <h1>Catalog</h1>
+
+      {catalog.map(product => (
+        <CatalogItem key={product.id} product={product}/>        
+      ))}
+    </main>
+  )
+}
